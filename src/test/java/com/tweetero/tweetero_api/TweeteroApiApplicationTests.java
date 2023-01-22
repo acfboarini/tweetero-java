690dda71b90ff1b2513282c895e72ce1e169fc36
augusto <acfboarini@gmail.com>
2023-01-22 03:41:13 -0300

    inicio

---
+++ src/test/java/com/tweetero/tweetero_api/TweeteroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweetero.tweetero_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeteroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
